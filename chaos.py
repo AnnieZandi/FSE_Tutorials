749b5e78e39112b4bcb0967b89b7701a442ff14b
AnnieZandi <annitazandile@gmail.com>
2026-02-23 13:20:45 +0200

    I formatted my code

--- chaos.py
+++ chaos.py
@@ -1,10 +1,9 @@
-
-
 def main():
     print("This programme illustrates a chaotic function")
     x = eval(input("Enter a number between 0 and 1:"))
-    for i in range(10): 
-        x = 3.9 * x *(1 - x)
+    for i in range(10):
+        x = 3.9 * x * (1 - x)
         print(x)
-main()
 
+
+main()
